fixing bugs in data preparation and accuracy calculation

data/data_pipe.py
@@ -90,7 +90,6 @@ def get_train_loader(conf):
         ds, class_num = get_train_dataset(conf.emore_folder/'imgs')
     elif conf.data_mode == 'age':
         ds, class_num = get_train_dataset(Path(conf.emore_folder/'imgs'),conf.age_train_file)
-        class_num = 85742
     loader = DataLoader(ds, batch_size=conf.batch_size, shuffle=True, pin_memory=conf.pin_memory, num_workers=conf.num_workers)
     return loader, class_num 
      
@@ -109,6 +108,7 @@ def load_lag(path, imgdir,rootdir, transform, image_size=[112,112]):
         if idx % 1000 == 0:
             print('loading ', idx)
     print(data.shape)
+    data.flush()
     issame_list = np.array([True]*(len(df)//2) + [False]*(len(df)//2))
     np.save(str(rootdir)+'_list', np.array(issame_list))
     return data, issame_list
@@ -129,6 +129,7 @@ def load_bin(path, rootdir, transform, image_size=[112,112]):
         if i % 1000 == 0:
             print('loading bin', i)
     print(data.shape)
+    data.flush()
     np.save(str(rootdir)+'_list', np.array(issame_list))
     return data, issame_list
 

utils.py
@@ -28,7 +28,7 @@ def separate_bn_paras(modules):
                 paras_wo_bn.extend([*layer.parameters()])
     return paras_only_bn, paras_wo_bn
 
-def prepare_facebank(conf, model, mtcnn, tta = True):
+def prepare_facebank(conf, model, mtcnn, tta = True, save = False):
     model.eval()
     embeddings =  []
     names = ['Unknown']
@@ -63,9 +63,10 @@ def prepare_facebank(conf, model, mtcnn, tta = True):
         embeddings.append(embedding)
         names.append(path.name)
     embeddings = torch.cat(embeddings)
-    names = np.array(names)
-    torch.save(embeddings, conf.facebank_path/'facebank.pth')
-    np.save(conf.facebank_path/'names', names)
+    if save:
+        names = np.array(names)
+        torch.save(embeddings, conf.facebank_path/'facebank.pth')
+        np.save(conf.facebank_path/'names', names)
     return embeddings, names
 
 def load_facebank(conf):

verifacation.py
@@ -36,7 +36,7 @@ def calculate_roc(thresholds, embeddings1, embeddings2, actual_issame, nrof_fold
     assert (embeddings1.shape[1] == embeddings2.shape[1])
     nrof_pairs = min(len(actual_issame), embeddings1.shape[0])
     nrof_thresholds = len(thresholds)
-    k_fold = KFold(n_splits=nrof_folds, shuffle=False)
+    k_fold = KFold(n_splits=nrof_folds, shuffle=True)
 
     tprs = np.zeros((nrof_folds, nrof_thresholds))
     fprs = np.zeros((nrof_folds, nrof_thresholds))
@@ -116,7 +116,7 @@ def calculate_val(thresholds, embeddings1, embeddings2, actual_issame, far_targe
     assert (embeddings1.shape[1] == embeddings2.shape[1])
     nrof_pairs = min(len(actual_issame), embeddings1.shape[0])
     nrof_thresholds = len(thresholds)
-    k_fold = KFold(n_splits=nrof_folds, shuffle=False)
+    k_fold = KFold(n_splits=nrof_folds, shuffle=True)
 
     val = np.zeros(nrof_folds)
     far = np.zeros(nrof_folds)